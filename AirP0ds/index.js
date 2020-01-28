e23feeaafb491026386e87112ae246e366214447
Joaco Fernandez <Joaco@Mouse.local>
2020-01-28 17:25:58 -0500

    issue

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -2,7 +2,5 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 
-  const number = JSON.stringify(github.number, undefined, 2)
-  console.log(`The event number: ${number}`);
-  const title = JSON.stringify(github.title, undefined, 2)
-  console.log(`The event title: ${title}`);
+  const issue = JSON.stringify(github.context.payload.issue, undefined, 2)
+  console.log(`The event issue: ${issue}`);
