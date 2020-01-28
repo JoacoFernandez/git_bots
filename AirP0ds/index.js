5a1e9a839af73d8972885a6d2b6bc532d934e125
Joaco Fernandez <Joaco@Mouse.local>
2020-01-28 18:05:33 -0500

    Gato loco

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -4,3 +4,12 @@ const github = require('@actions/github');
 
   const issue = github.context.payload.issue
   console.log(`The issue: #${issue.number} ${issue.title}`);
+  const myToken = core.getInput('repo-token');
+  const octokit = new github.GitHub(myToken);
+  body = "![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)"
+  octokit.issues.createComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issue.number,
+    body: body
+  })
