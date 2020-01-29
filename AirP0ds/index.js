215bea69e53c6ab38515b6030d16db4137a03d91
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-01-29 16:53:24 -0300

    420

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -8,8 +8,8 @@ const github = require('@actions/github');
   const myToken = core.getInput('repo-token');
   const octokit = new github.GitHub(myToken);
   body = "![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)"
-  var checklist_item_1 ="- [x] Updated `fastlane-plugin-test_center` to the latest version"
-  var checklist_item_2 ="- [x] I read the [README.md](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md)"
+  const checklist_item_1 ="- [x] Updated `fastlane-plugin-test_center` to the latest version"
+  const checklist_item_2 ="- [x] I read the [README.md](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md)"
   if (issue.body === "") {
    octokit.issues.update({
      owner: github.context.repo.owner,
@@ -26,10 +26,21 @@ const github = require('@actions/github');
 
   }
   else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)) {
+    var comment
+    if (!issue.body.includes(checklist_item_1) &&  !issue.body.includes(checklist_item_2)) {
+      comment = "Please make sure that you update fastlane to the latest version." + "Please make sure that read the README.md"
+    }
+    else if (!issue.body.includes(checklist_item_1)) {
+      comment = "Please make sure that you update fastlane to the latest version."
+    }
+    else (! issue.body.includes(checklist_item_2)){
+      comment = "Please make sure that read the README.md"
+    }
+
     octokit.issues.createComment({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       issue_number: issue.number,
-      body: "something is wrong."
+      body: comment
     })
   }
