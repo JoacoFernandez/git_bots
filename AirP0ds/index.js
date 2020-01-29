bf65fd0219d175a2ec52aade6149132b72167dec
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-01-29 12:58:54 -0300

    Hola

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -3,13 +3,14 @@ const github = require('@actions/github');
 
 
   const issue = github.context.payload.issue
-  console.log(`The issue: #${issue.number} ${issue.title}`);
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event issue: ${issue}`);
   const myToken = core.getInput('repo-token');
   const octokit = new github.GitHub(myToken);
   body = "![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)"
-  octokit.issues.update({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: issue.number,
-    state: "closed"
-  })
+  //octokit.issues.update({
+    //owner: github.context.repo.owner,
+    //repo: github.context.repo.repo,
+    //issue_number: issue.number,
+    //state: "closed"
+  //})
