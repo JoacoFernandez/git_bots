8ba93ff4510aac3eaad657f794f946f9f52ff8d9
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-01-30 11:03:51 -0300

    Perfect work

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -11,6 +11,7 @@ const github = require('@actions/github');
   const checklist_item_1 ="- [x] Updated `fastlane-plugin-test_center` to the latest version"
   const checklist_item_2 ="- [x] I read the [README.md](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md)"
   const checklist_item_3 ="- [x] I reviewed the [example(s)](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md) for the action(s) I am using"
+  const checklist_item_4 ="- [x] I have removed any sensitive data such as passwords, authentication tokens, or anything else I do not want to world to see"
   if (issue.body === "") {
    octokit.issues.update({
      owner: github.context.repo.owner,
@@ -26,10 +27,10 @@ const github = require('@actions/github');
    })
 
   }
-  else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)||!issue.body.includes(checklist_item_3)) {
+  else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)||!issue.body.includes(checklist_item_3)||!issue.body.includes(checklist_item_4)) {
     var comment
-    if (!issue.body.includes(checklist_item_1) && !issue.body.includes(checklist_item_2) && !issue.body.include(checklist_item_3)) {
-      comment = "Please make sure that you update fastlane to the latest version." + "\n" + "Please make sure that read the README.md" + "\n" + "Please make sure that you have reviewed the documentation for the action you are using"
+    if (!issue.body.includes(checklist_item_1) && !issue.body.includes(checklist_item_2) && !issue.body.include(checklist_item_3) && !issue.body.include(checklist_item_4)) {
+      comment = "Please make sure that you update fastlane to the latest version." + "\n" + "Please make sure that read the README.md" + "\n" + "Please make sure that you have reviewed the documentation for the action you are using" + "\n" + "Please make sure that you have removed all sensitive data, otherwise people may use that data in ways that cause harm."
     }
     else if (!issue.body.includes(checklist_item_1)) {
       comment = "Please make sure that you update fastlane to the latest version."
@@ -39,6 +40,9 @@ const github = require('@actions/github');
     }
     else if (! issue.body.includes(checklist_item_3)){
       comment = "Please make sure that you have reviewed the documentation for the action you are using"
+    }
+    else if (! issue.body.includes(checklist_item_4)){
+      comment = "Please make sure that you have removed all sensitive data, otherwise people may use that data in ways that cause harm."
     }
 
     octokit.issues.createComment({
