20273c77f0e6dc652b97770caabcd17b90d67beb
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-01-30 12:42:27 -0300

    iou

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -12,6 +12,7 @@ const github = require('@actions/github');
   const checklist_item_2 ="- [x] I read the [README.md](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md)"
   const checklist_item_3 ="- [x] I reviewed the [example(s)](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md) for the action(s) I am using"
   const checklist_item_4 ="- [x] I have removed any sensitive data such as passwords, authentication tokens, or anything else I do not want to world to see"
+
   if (issue.body === "") {
    octokit.issues.update({
      owner: github.context.repo.owner,
@@ -28,7 +29,7 @@ const github = require('@actions/github');
 
   }
   else if (!issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)||!issue.body.includes(checklist_item_3)||!issue.body.includes(checklist_item_4)) {
-    var comment = ""
+    var comment = "#THERE ARE SOME PROBLEMS"
     if (!issue.body.includes(checklist_item_1)){
       comment = comment + "_**Please** make sure that you update fastlane to the latest version._"
     }
