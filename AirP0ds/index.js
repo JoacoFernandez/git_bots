aa7ee8e25013284a38d7005cf694c787978248ba
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-01-30 10:44:07 -0300

    Woho

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -26,9 +26,9 @@ const github = require('@actions/github');
    })
 
   }
-  else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)||(!checklist_item_3)) {
+  else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)||!issue.body.includes(checklist_item_3)) {
     var comment
-    if (!issue.body.includes(checklist_item_1) &&  !issue.body.includes(checklist_item_2) && (checklist_item_3)) {
+    if (!issue.body.includes(checklist_item_1) && !issue.body.includes(checklist_item_2) && !issue.body.include(checklist_item_3)) {
       comment = "Please make sure that you update fastlane to the latest version." + "\n" + "Please make sure that read the README.md" + "\n" + "Please make sure that you have reviewed the documentation for the action you are using"
     }
     else if (!issue.body.includes(checklist_item_1)) {
