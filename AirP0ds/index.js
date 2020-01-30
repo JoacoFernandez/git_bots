234910ba4a32a566a3edc3f1009b59b7e663d406
Joaquin Fernandez Abadie <jfernandezabadie@udesa.edu.ar>
2020-01-30 10:35:06 -0300

    420

--- AirP0ds/index.js
+++ AirP0ds/index.js
@@ -10,6 +10,7 @@ const github = require('@actions/github');
   body = "![Image of Yaktocat](https://octodex.github.com/images/yaktocat.png)"
   const checklist_item_1 ="- [x] Updated `fastlane-plugin-test_center` to the latest version"
   const checklist_item_2 ="- [x] I read the [README.md](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md)"
+  const checklist_item_3 ="- [x] I reviewed the [example(s)](https://github.com/lyndsey-ferguson/fastlane-plugin-test_center/blob/master/README.md) for the action(s) I am using "
   if (issue.body === "") {
    octokit.issues.update({
      owner: github.context.repo.owner,
@@ -25,7 +26,7 @@ const github = require('@actions/github');
    })
 
   }
-  else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)) {
+  else if ( !issue.body.includes(checklist_item_1)||!issue.body.includes(checklist_item_2)||(!checklist_item_3)) {
     var comment
     if (!issue.body.includes(checklist_item_1) &&  !issue.body.includes(checklist_item_2)) {
       comment = "Please make sure that you update fastlane to the latest version." + "\n" + "Please make sure that read the README.md"
@@ -35,6 +36,9 @@ const github = require('@actions/github');
     }
     else if (! issue.body.includes(checklist_item_2)){
       comment = "Please make sure that read the README.md"
+    }
+    else if (! issue.body.includes(checklist_item_3)){
+      comment = "Please make sure that you have reviewed the documentation for the action you are using"
     }
 
     octokit.issues.createComment({
